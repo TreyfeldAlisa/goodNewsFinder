69853fe11711872ffc8e1948b805eadd37565dbb
Maksim Kharlamov <98958340+Daznot@users.noreply.github.com>
2022-07-06 16:22:37 +0300

    DataBase - exelent (#4)

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
+require('dotenv').config();
+require('@babel/register');
+
 const express = require('express');
+const config = require('./config/config');
+const PORT = process.env.PORT ?? 3000;
+
+// импортировать роутеры
 
 const app = express();
 
-const PORT = process.env.PORT ?? 3000;
+config(app);
+
+// подключить роутеры
 
 app.listen(PORT, () => {
-    console.log('Вологодский ОМОН работает на', PORT);
-  });
+  console.log('Веб-сервер слушает порт', PORT);
+});
