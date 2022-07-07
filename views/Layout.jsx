a5adc06c30eda25ee0a70b1eeedc70969026da1c
Alisa Treyfeld <treyfeld@yandex.ru>
2022-07-07 15:11:06 +0300

    Bag fixing - rendering reg/log

--- views/Layout.jsx
+++ views/Layout.jsx
@@ -1,8 +1,8 @@
 const React = require('react');
 
-const Navbar = require('./Navbar');
+const Navbar = require('./NavBar');
 
-module.exports = function Layout({ children, user }) {
+module.exports = function Layout({ children }) {
   return (
     <html lang="en">
       <head>
@@ -12,10 +12,10 @@ module.exports = function Layout({ children, user }) {
         <link type="image/png" size="64x64" href="/icons8.png" rel="icon" />
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossOrigin="anonymous" />
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossOrigin="anonymous" />
+        <script src="/js/applications.js" defer />
         <title>Good news</title>
       </head>
       <body>
-        <Navbar user={user} />
         {children}
       </body>
 
