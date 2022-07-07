8440802c627b8dfe716eb71c86aeb08a5a3c63c9
Alisa Treyfeld <104518603+TreyfeldAlisa@users.noreply.github.com>
2022-07-07 13:03:43 +0300

    Favicon (#6)
    * favicon
    
    * favicon done

--- views/Layout.jsx
+++ views/Layout.jsx
@@ -9,7 +9,7 @@ module.exports = function Layout({ children, user }) {
         <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link type="image/x-icon" sizes="16x16" href="/public/favicon.ico" rel="icon" />
+        <link type="image/png" size="64x64" href="/icons8.png" rel="icon" />
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossOrigin="anonymous" />
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossOrigin="anonymous" />
         <title>Good news</title>
